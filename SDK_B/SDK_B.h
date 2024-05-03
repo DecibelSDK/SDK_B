a92c42cfd15014f388bc9727a69d1219656d3a1b
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-03 11:17:27 +0200

    Initial Commit

---
+++ SDK_B/SDK_B.h
@@ -0,0 +1,18 @@
+//
+//  SDK_B.h
+//  SDK_B
+//
+//  Created by victor iglesias on 3/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK_B.
+FOUNDATION_EXPORT double SDK_BVersionNumber;
+
+//! Project version string for SDK_B.
+FOUNDATION_EXPORT const unsigned char SDK_BVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK_B/PublicHeader.h>
+
+
