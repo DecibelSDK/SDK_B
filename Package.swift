0daccabb10707d77e2f65920f849102969cae4ab
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-07 12:45:43 +0200

    1.0.1

--- Package.swift
+++ Package.swift
@@ -14,21 +14,12 @@ let package = Package(
         .library(
             name: "sdk-b",
             targets: ["SDK_B"]
-        ),
-        .library(
-            name: "sdk-br",
-            targets: ["SDK_BR"]
-        ),
+        )
     ],
     targets: [
         .binaryTarget(
             name: "SDK_B",
             path: "SDK_B.xcframework"
-        ),
-        .binaryTarget(
-            name: "SDK_BR",
-            url: "https://github.com/DecibelSDK/SDK_BR/releases/download/1.0.1/SDK_BR.xcframework.zip",
-            checksum: "5bc54cca3f15b7dea50e83266475e9fcda53777e2856abb5a9c9617533c98cf2"
-        ),
+        )
     ]
 )
