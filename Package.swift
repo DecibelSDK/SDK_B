c3d8cc603cca1076e4920ab60b95a226d62c649c
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-07 15:43:38 +0200

    1.0.2

--- Package.swift
+++ Package.swift
@@ -13,13 +13,34 @@ let package = Package(
     products: [
         .library(
             name: "sdk-b",
-            targets: ["SDK_B"]
+            targets: ["SDK_B_Wrapper"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/DecibelSDK/SDK_BR.git",
+            from: "1.0.2"
         )
     ],
     targets: [
         .binaryTarget(
             name: "SDK_B",
             path: "SDK_B.xcframework"
+        ),
+        .target(
+            name: "SDK_B_Wrapper",
+            dependencies: [
+                .target(
+                    name: "SDK_B",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "sdk-br",
+                    package: "SDK_BR",
+                    condition: .when(platforms: [.iOS])
+                )
+            ],
+            path: "SDK_B_Wrapper"
         )
     ]
 )
